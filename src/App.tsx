052bd95f69d164502be944667903968814096c99
bwuphan <bwuphan@gmail.com>
2020-10-19 20:50:45 -0700

    Add CreateList

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,23 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 
-import logo from './logo.svg';
 import './App.css';
+
+import CreateList from './components/CreateList/CreateList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/create">
+            <CreateList />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
