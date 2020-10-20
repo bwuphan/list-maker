974265984be6cfc10d5156196f8da7f68d477c83
bwuphan <bwuphan@gmail.com>
2020-10-19 21:35:19 -0700

    WIP: adding appendToList and changeInput

--- src/components/CreateList/CreateList.tsx
+++ src/components/CreateList/CreateList.tsx
@@ -3,15 +3,24 @@ import styles from './CreateList.module.scss';
 
 
 const CreateList: React.FC = () => {
-  const [list, setState] = useState(['']);
+  let [list, setState] = useState(['']);
 
   const appendToList = () => {
     list.push('');
     console.log(list);
+    list = [...list];
     setState(list);
+  }
+
+  const changeInput = (e: any, idx: number) => {
+    list[idx] = list[idx] + e.target.value;
+    console.log(list);
   }
   return (
     <div>
+      {list.map((item, i) =>
+        <input value={item} onChange={(e) => changeInput(e, i)}/>
+      )}
       <p>You clicked {list} times</p>
       <button onClick={appendToList}>
         Click me
