251ab41f20eb7b59754830d9bd1a18e1b69f5252
bwuphan <bwuphan@gmail.com>
2020-10-26 23:18:40 -0700

    Add changeOrder

--- src/components/CreateList/CreateList.tsx
+++ src/components/CreateList/CreateList.tsx
@@ -19,17 +19,35 @@ const CreateList: React.FC = () => {
     // setList([...list.slice(0, idx), e.target.value, ...list.slice(idx)])
   }
 
+  const changeOrder = (e: any, idx: number) => {
+    const movedItem = list[idx];
+    let newList = [...list];
+    newList.splice(idx, 1);
+    newList.splice(e.target.value, 0, movedItem);
+    setList(newList);
+  }
+
   return (
     <div>
       <div className="d-flex flex-column w-50 mx-auto">
         {list.map((item, i) =>
-          <InputGroup>
-            <select className="w-25">
+          <InputGroup key={i}>
+            <select
+              className="w-25"
+              defaultValue={i}
+              onChange={(e) => changeOrder(e, i)}
+            >
               {Array.from(Array(list.length), (x, j) => {
                 return <option key={j} value={j} selected={j === i}>{j + 1}</option>
               })}
             </select>
-            <input className="w-75" type="text" key={i} value={item} onInput={(e) => changeInput(e, i)} />
+            <input
+              className="w-75"
+              type="text"
+              key={i}
+              value={item}
+              onInput={(e) => changeInput(e, i)}
+            />
           </InputGroup>
         )}
         <Button onClick={appendToList}>
